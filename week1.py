4a5a5fb21f6129f220d28c7a1c586596d92d07e5
Devakh Rashie <59419810+arkanemystic@users.noreply.github.com>
2025-09-22 13:57:06 -0400

    Create week1.py

---
+++ week1.py
@@ -0,0 +1,12 @@
+# 1. Import the ccxt library
+
+# 2. Create an "instance" of the Coinbase exchange
+
+# 3. Define the trading symbols we want data for
+#    (Note: a "ticker" symbol is just a pair like BTC/USD)
+
+# 4. Use the fetch_ticker() function to get the latest market data
+
+# 5. The data is a dictionary. The price is stored in the 'last' key.
+
+# 6. Print the results in a readable format!
